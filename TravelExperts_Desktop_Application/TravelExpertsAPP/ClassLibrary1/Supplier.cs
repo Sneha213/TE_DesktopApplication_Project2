a15e82a1facc1331783560a3db3e83e609710238
Sneha Patel <snehapatel882@gmail.com>
2018-09-12 13:34:46 -0600

    Project2 Done

---
+++ TravelExperts_Desktop_Application/TravelExpertsAPP/ClassLibrary1/Supplier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelExpertsLibrary
+{
+    // Business(Entity) Class for Supplier - Middle layer
+    public class Supplier
+    {
+        public Supplier() { }
+
+        public int SupplierId { get; set; }
+
+        public string SupName { get; set; }
+    }
+}
